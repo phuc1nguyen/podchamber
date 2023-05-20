737a6a0228b16bf21d396348dd18560af0642835
phuc1nguyen <phuc.ng13988@gmail.com>
2023-05-20 11:34:23 +0700

    :tada: Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# PodChamber
+
+## User stories
+
+* [ ] User can see a table of podcast episodes
+* [ ] User can see rows in this table showing a clickable episode icon, the title of the episode, and the date it was originally broadcast
+* [ ] User can scroll through the list
+* [ ] User can click on the episode icon to display that episodes page on the Podbean web site
+
+## Extended features
+
+* [ ] User can see rows in the episode table have alternating background colors
+* [ ] User can see a summary above the episode table showing the number of episodes for each podcast
+* [ ] User can see a clickable checkbox next to each podcast name in the summary above the episode table
+* [ ] User can click the radio button next to the podcast name to include episodes for that podcast in the episode table
---
+++ index.js
@@ -0,0 +1,27 @@
+import puppeteer from 'puppeteer';
+
+const init = async () => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const browser = await puppeteer.launch({ headless: 'new' });
+      const page = await browser.newPage();
+      await page.goto(
+        'https://www.podbean.com/podcast-detail/d4un8-57595/JavaScript-Jabber-Podcast'
+      );
+      let episodeLinks = await page.evaluate(() => {
+        return Array.from(document.querySelectorAll('a.title')).map((item) => ({
+          url: item.getAttribute('href'),
+          text: item.innerText,
+        }));
+      });
+      browser.close();
+      return resolve(episodeLinks);
+    } catch (e) {
+      return reject(e);
+    }
+  });
+};
+
+init()
+  .then((res) => console.log(res))
+  .catch((err) => console.error(err));
